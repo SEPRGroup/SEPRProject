96c2281548f698187e66032f5bc4b815b5171076
pgs505 <pdasquires@hotmail.co.uk>
2013-11-19 12:02:02 +0000

    Skeleton Flight Class
    Class with all variables and methods - methods are public void and have
    no body yet. Most Variables are user defined data types which haven't
    been defined yet - e.g. status is type FlightStatus.

---
+++ Code/Flight.java
@@ -0,0 +1,49 @@
+class Flight {
+	// variables
+	public String identifier;
+	public FlightStatus status;  //FlightStatus is a user defined data type
+	public Vector position;		 //Vector is a user defined data type
+	public Queue flightPlan;	 //Queue **might** be a user defined data type
+	public Conditions condition; //Conditions is a user defined data type
+
+	//methods
+	public void takeOff(TransferWaypoint t) { 	//TransferWaypoint is a user defined data type
+		//method will go in here
+	}
+
+	public void land(TransferWaypoint t) { 		
+		//method will go in here
+	}
+
+	public void turnTo(Bearing b) {				//Bearing is a user defined data type
+		//method will go in here
+	}
+
+	public void toAltitude(Real altitude) {
+		//method will go in here
+	}
+
+	public void nextWaypoint() {
+		//method will go in here
+	}
+
+	public void toSpeed(Real speed) {
+		//method will go in here
+	}
+
+	public void abort() {
+		//method will go in here
+	}
+
+	public void crash() {
+		//method will go in here
+	}
+
+	public void draw(Vector location; int size) {
+		//method will go in here
+	}
+
+	public void update(Time t) { 				//Time **might** be a user defined data type
+		//method will go in here
+	}
+}
