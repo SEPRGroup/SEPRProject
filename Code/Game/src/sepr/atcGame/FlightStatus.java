56915c75d4dbfac45f0c3d80b743ba0ee696c342
cjf521 <cjf521@york.ac.uk>
2013-11-27 09:05:51 +0000

    FlightStatus enum added
    Initial set of FlightStatuses added; everything I can think we need for
    a plane to be able to respond and signal appropriately for now. Add as
    necessary.

--- Code/Game/src/sepr/atcGame/FlightStatus.java
+++ Code/Game/src/sepr/atcGame/FlightStatus.java
@@ -1,6 +1,19 @@
 package sepr.atcGame;
 
-
-class FlightStatus {
-	//enumerations (Charlie's on this - Pat will help if needed)
+public enum FlightStatus {
+	WAITING ("No orders"),
+	TAXIING ("Taxiing"),
+	CRUISING ("Cruising"),
+	TAKEOFF ("Taking off"),
+	LANDING ("Landing"),
+	COMPLYING ("Following orders"),
+	CRASHING ("Crashed");
+	
+	private final String labelText;
+	FlightStatus(String text) {
+		this.labelText = text;
+	}
+	
+	public String labelText() { return labelText; }
+		
 }
