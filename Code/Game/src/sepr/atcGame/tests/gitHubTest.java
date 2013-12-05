273d4e5e15b3c3be703564db3d7e2140ebc995ec
Stephen Jenkins <sj771@york.ac.uk>
2013-12-05 15:44:48 +0000

    Test for github and creating a new branch

---
+++ Code/Game/src/sepr/atcGame/tests/gitHubTest.java
@@ -0,0 +1,13 @@
+package sepr.atcGame.tests;
+
+public class gitHubTest {
+	private int test = 5;
+	private String input;
+	
+	public gitHubTest(int test, String input)
+	{
+		this.test = test;
+		this.input = input;
+	}
+
+}
