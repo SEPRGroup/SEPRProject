d9873e570fdecb5f609be16eb5755d6de510f351
Stephen Jenkins <sj771@york.ac.uk>
2014-01-13 20:52:52 +0000

    Forgot to get Rid of @Ignore
    Still works however

--- Code/Game/src/sepr/atcGame/tests/ATCTest.java
+++ Code/Game/src/sepr/atcGame/tests/ATCTest.java
@@ -2,18 +2,36 @@ package sepr.atcGame.tests; import sepr.atcGame.*;
 import static org.junit.Assert.*;
 
 import org.junit.After;
-import org.junit.Test; import org.junit.Ignore; @Ignore
+import org.junit.Before;
+import org.junit.Test; import org.junit.Ignore; 
 
 public class ATCTest {
 	
 	public ATC instance;
 	public String newATCName;
 	public Airspace newAirspace;
-
+	
+	@Before
+	public void setup() {
+		newATCName = "ATC";
+		newAirspace = new Heathrow();
+		instance = new ATC(newATCName, newAirspace);
+	}
+	
+	// I'm thinking this could be instead of the setup() function
+	@Test
+	public void testATC() {
+		
+		
+		
+		assertEquals(newATCName, instance.getName());
+		assertEquals(newAirspace, instance.getAirspace());
+	}
+	
 	@Test
 	public void testGetName() {
 		
-		assertEquals(newATCName, instance.getName(), 0);
+		assertEquals("ATC", instance.getName());
 	}
 
 	@Test
@@ -21,16 +39,7 @@ public class ATCTest {
 		assertEquals(newAirspace, instance.getAirspace());
 	}
 
-	// I'm thinking this could be instead of the setup() function
-	@Test
-	public void testATC() {
-		newATCName = "ATC";
-		newAirspace = new Heathrow();
-		instance = new ATC(newATCName, newAirspace);
-		
-		assertEquals(newATCName, instance.getName(), 0);
-		assertEquals(newAirspace, instance.getAirspace());
-	}
+	
 
 	@After
 	public void tearDown(){
