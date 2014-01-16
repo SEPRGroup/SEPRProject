7f986d4af52ec4086ed2f18ac3d25be6ffa46eb3
steviej08 <stephen.jenkins@solm.org>
2014-01-16 12:41:23 +0000

    Changed background of labels
    Need to make the background fit properly

--- Code/Game/src/sepr/atcGame/UI/MenuButton.java
+++ Code/Game/src/sepr/atcGame/UI/MenuButton.java
@@ -11,16 +11,18 @@ import sepr.atcGame.UI.Main;
 
 public class MenuButton extends JLabel {
 	
-	private Font font = new Font("Bauhaus 93",Font.PLAIN, 50);
-	
-	
+	private Font font = new Font("Bauhaus 93",Font.PLAIN, 20);
+	private Color color = new Color(0,0,255);
+	private ImageIcon cloud = new ImageIcon("src/sepr/atcGame/Images/cloud.png");
 	
 	public MenuButton(String textName)
 	{		
 		this.setText(textName);	
-		this.setBackground(Color.white);
+		this.setBackground(color);
+		this.setIcon(cloud);
+		this.setIconTextGap(-75);
 		this.setFont(font);	
-		this.setForeground(Color.white);
+		this.setForeground(color);
 		this.setHorizontalAlignment(SwingConstants.CENTER);
 		this.setVerticalAlignment(SwingConstants.CENTER);
 	
