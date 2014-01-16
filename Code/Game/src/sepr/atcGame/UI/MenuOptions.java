082154d92c3a7d183cc6f0552dd4290779826a91
Zivile <zl728@york.ac.uk>
2014-01-16 03:21:59 +0000

    changed gui

--- Code/Game/src/sepr/atcGame/UI/MenuOptions.java
+++ Code/Game/src/sepr/atcGame/UI/MenuOptions.java
@@ -1,22 +1,18 @@
 package sepr.atcGame.UI;
 
 public enum MenuOptions {
-	START (new MenuButton("START")),
-	PLAY (new MenuButton("Play")), 
-	CHOOSEAIRPORT (new MenuButton("Choose Airport")),
-	SELECTDIFFICULTY (new MenuButton("Select Difficulty")),
-	EXIT (new MenuButton("Exit")),
-	EASY (new MenuButton("Easy")),
-	MEDIUM (new MenuButton("Medium")),
-	HARD (new MenuButton("Hard")),
-	AIRPORT1 (new MenuButton("Airport 1")),
-	AIRPORT2 (new MenuButton("Airport 2")),
-	BACK (new MenuButton("Back"));
-	
-	public final MenuButton menuButton;
-	MenuOptions(MenuButton menuButton) {
-		this.menuButton = menuButton;
-	}
+		START (new MenuButton("START")),
+		PLAY (new MenuButton("Play")), 
+		CHOOSEAIRPORT (new MenuButton("Choose Airport")),
+		AIRPORT1 (new MenuButton("Heathrow")),
+		AIRPORT2 (new MenuButton("Airport 2")),
+		EXIT (new MenuButton("Exit"));
 		
-	
+
+	    public final MenuButton menuButton;
+		MenuOptions(MenuButton menuButton) {
+			this.menuButton = menuButton;
+		}
+
+
 }
