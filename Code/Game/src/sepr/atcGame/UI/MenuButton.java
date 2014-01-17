d05ac31f8e06904a98e9f99cf87f36c1b33928d5
cjf521 <cjf521@york.ac.uk>
2014-01-17 14:55:44 +0000

    MenuButton background fixed
    -MenuButton background sizes and positions correctly

--- Code/Game/src/sepr/atcGame/UI/MenuButton.java
+++ Code/Game/src/sepr/atcGame/UI/MenuButton.java
@@ -1,71 +1,59 @@
 package sepr.atcGame.UI;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-
-
 public class MenuButton extends JPanel {
-	
-	
-	
-	private Image img = new ImageIcon("src/sepr/atcGame/Images/menu_label.png").getImage();
-	private FontMetrics fm;
+
 	private Image scaleBackground;
-	private Dimension size;
 	private MenuText menuText;
-	
+
+	private static Image img;
+
+
 	public MenuButton(String textName)
-	{		
+	{	
 		setLayout(new FlowLayout(FlowLayout.CENTER, 20, 20));
-		
+		setOpaque(false);
+
 		menuText = new MenuText(textName);
-		
-		this.fm = menuText.getFontMetrics(menuText.getFont());
-		size = new Dimension(fm.getHeight(), fm.stringWidth(textName));	
+
 		add(menuText);
-		repaint();
 	}
-	
+
 	public MenuText getMenuText(){
 		return menuText; 
 	}
-	
+
 	@Override
 	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		
-		
-		Rectangle bounds = getBounds();
-		
-		
-		if (scaleBackground != null){			
-			g.drawImage(scaleBackground, 0, 0, null);}
-		else if(img != null){
-			scaleBackground = img.getScaledInstance(bounds.width, bounds.height, Image.SCALE_SMOOTH);
-			g.drawImage(scaleBackground, 0, 0, null);}	
-		else{
-			g.setColor(Color.BLACK);
-			g.fillRect(bounds.x, bounds.y, bounds.width, bounds.height); 
+		if (scaleBackground == null){
+			if (img == null){
+				try {img = ImageIO.read(new File("src/sepr/atcGame/Images/menu_label.png"));}
+				catch (IOException e){};
+			}
+			scaleBackground = img.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
 		}
+
+		g.drawImage(scaleBackground, 0, 0, null);
 	}
-	
-	class MenuText extends JLabel {
-		
+
+	public class MenuText extends JLabel {
+
 		private Color color = new Color(0,0,255);
 		private Font font = new Font("Bauhaus 93",Font.PLAIN, 35);
-		
-		
+
 		public MenuText(String text){
+			super(text);
 			this.setForeground(color);
 			this.setFont(font);
-			this.setText(text);
-			this.setVisible(true);
 		}
-		
+
 	}
-	
-		
+
 }
 
