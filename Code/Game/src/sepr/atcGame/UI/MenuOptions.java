09a08ab2209afc6f9d8db2053f246bc3bffc0126
Karl Sonley <kms526@york.ac.uk>
2014-01-17 16:32:01 +0000

    Menu Graphics Update
    + Added image snippet of Heathrow gameplay area
    + Added shadow to menu backgrounds to make them stand out more
    - Added spaces to menu words to make background scale properly

--- Code/Game/src/sepr/atcGame/UI/MenuOptions.java
+++ Code/Game/src/sepr/atcGame/UI/MenuOptions.java
@@ -1,16 +1,16 @@
 package sepr.atcGame.UI;
 
 public enum MenuOptions {
-		START (new MenuButton("START")),
+		START (new MenuButton(" START ")),
 		PLAY (new MenuButton("Play")),
-		CHOOSEAIRPORT (new MenuButton("Choose Airport")),
-		SELECTDIFFICULTY (new MenuButton("Select Difficulty")),
+		CHOOSEAIRPORT (new MenuButton("   Choose Airport   ")),
+		SELECTDIFFICULTY (new MenuButton("    Select Difficulty    ")),
 		EXIT (new MenuButton("Exit")),
 		EASY (new MenuButton("Easy")),
-		MEDIUM (new MenuButton("Medium")),
+		MEDIUM (new MenuButton(" Medium ")),
 		HARD (new MenuButton("Hard")),
-		AIRPORT1 (new MenuButton("Heathrow")),
-		AIRPORT2 (new MenuButton("Airport 2")),
+		AIRPORT1 (new MenuButton(" Heathrow ")),
+		AIRPORT2 (new MenuButton(" Airport 2 ")),
 		BACK (new MenuButton("Back"));
 		
 
