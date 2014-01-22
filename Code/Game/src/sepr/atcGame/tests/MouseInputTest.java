11a0f6fdc42ddfc378d4a3d43429b3fc73d01ad9
Jake Digweed <jwd503@york.ac.uk>
2014-01-22 03:36:20 +0000

    Added comment to MouseInputTest

--- Code/Game/src/sepr/atcGame/tests/MouseInputTest.java
+++ Code/Game/src/sepr/atcGame/tests/MouseInputTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test; import org.junit.Ignore; @Ignore
 
 public class MouseInputTest {
-
+	//Can't be tested with unit testing as this requires comparing graphics
 	@Test
 	public void testMouseInput() {
 		fail("Not yet implemented");
