71ec21999eda4dd802b4fb31d018395d23ecd2ae
Jake Digweed <jwd503@york.ac.uk>
2014-01-22 02:06:10 +0000

    Fixed HeathrowTest
    GenerateWaypoints test fixed

--- Code/Game/src/sepr/atcGame/tests/HeathrowTest.java
+++ Code/Game/src/sepr/atcGame/tests/HeathrowTest.java
@@ -15,7 +15,7 @@ import org.junit.Test;
 public class HeathrowTest {
 
 	public Heathrow instance;
-	public final int MAX_WAYPOINTS = 5;
+	public final int MAX_WAYPOINTS = 10;
 	Waypoint[] newWaypoints;
 	
 	@Before
@@ -27,27 +27,33 @@ public class HeathrowTest {
 	
 	@Test
 	// Generate the waypoints again and test them against the already generated waypoints in heathrow
-	// Use assertArrayEquals
+	
 	public void testGenerateWaypoints() {
 		
 		newWaypoints = new Waypoint[MAX_WAYPOINTS];
 		
+		newWaypoints[0] = new Waypoint("Alpha", new Position(2200,3000,3000));
+	    newWaypoints[1] = new Waypoint("Bravo", new Position(4000,6140,3000));
+		newWaypoints[2] = new Waypoint("Charlie", new Position(12000,6140,3000));
+		newWaypoints[3] = new Waypoint("Delta", new Position(7000,10400,3000));
+		newWaypoints[4] = new Waypoint("Echo", new Position(13000,2000,3000));
+		newWaypoints[5] = new Waypoint("Foxtrot", new Position(5380,2200,3000));
+		newWaypoints[6] = new Waypoint("Golf", new Position(1840,8660,3000));
+		newWaypoints[7] = new Waypoint("Hotel", new Position(14040,8480,3000));
+		newWaypoints[8] = new Waypoint("Indigo", new Position(10200,8260,3000));
+		newWaypoints[9] = new Waypoint("Juliett", new Position(10100,2600,3000));
 		
-		for(int i = 0 ; i < MAX_WAYPOINTS; i++){
-			newWaypoints[i] = new Waypoint("waypoint" +i,
-					new Position((i)*instance.getWidth()/(MAX_WAYPOINTS)+400, (Math.sin(Math.toRadians(i*360/MAX_WAYPOINTS))+1)*(instance.getHeight()/3)+200, 5000) );
-			System.out.println("Dummy Waypoint: " + newWaypoints[i]);
-			
-		}
-		for (Waypoint w: instance.getWaypoints()){
-			System.out.println("Actual Waypoint:" + w);
-		}
+		assertEquals(newWaypoints[0].toString(), instance.getWaypoints()[0].toString());
+		assertEquals(newWaypoints[1].toString(), instance.getWaypoints()[1].toString());
+		assertEquals(newWaypoints[2].toString(), instance.getWaypoints()[2].toString());
+		assertEquals(newWaypoints[3].toString(), instance.getWaypoints()[3].toString());
+		assertEquals(newWaypoints[4].toString(), instance.getWaypoints()[4].toString());
+		assertEquals(newWaypoints[5].toString(), instance.getWaypoints()[5].toString());
+		assertEquals(newWaypoints[6].toString(), instance.getWaypoints()[6].toString());
+		assertEquals(newWaypoints[7].toString(), instance.getWaypoints()[7].toString());
+		assertEquals(newWaypoints[8].toString(), instance.getWaypoints()[8].toString());
+		assertEquals(newWaypoints[9].toString(), instance.getWaypoints()[9].toString());
 		
-		// Not sure why this isn't working as they are exactly the same
-		assertArrayEquals(newWaypoints, instance.getWaypoints());
-		//Heathrow has changed; Waypoints are no longer the same
-		//in addition, Waypoint does not redefine .equals(), so it will use the default method of comparing memory addresses
-			//this will fail as they are separately generated objects
 	}
 
 	// Not sure how to check if the image came in??
